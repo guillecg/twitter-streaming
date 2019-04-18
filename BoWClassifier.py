26d15a394b4c6cbdc8ccd895dbafde75f632b921
Guillermo Climent <guillermo.climent.gargallo@gmail.com>
2019-04-18 15:12:57 +0200

    Add plots for visualizing word cloud and sentiment count

--- BoWClassifier.py
+++ BoWClassifier.py
@@ -1,6 +1,9 @@
 import re
 
 import numpy as np
+import pandas as pd
+
+from collections import Counter
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -12,6 +15,13 @@ from sklearn.pipeline import Pipeline
 from textblob import TextBlob
 
 from pymongo import MongoClient
+
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+sns.set(style='white')
+
+from wordcloud import WordCloud
 
 
 # https://www.geeksforgeeks.org/twitter-sentiment-analysis-using-python/
@@ -55,15 +65,53 @@ class BoWClassifier(TextPreprocesser):
         pass
 
 
+def plot_wordcloud(text):
+
+    # Generate a word cloud image
+    wordcloud = WordCloud(
+        width=1600, height=1200
+    ).generate(' '.join(text))
+    plt.figure(figsize=(20,10))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+
+    # lower max_font_size
+    wordcloud = WordCloud(
+        width=800, height=400, max_font_size=50
+    ).generate(' '.join(text))
+    plt.figure(figsize=(20,10))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+
+    # Show the plots
+    plt.show()
+
+
 if __name__ == '__main__':
     client = MongoClient('localhost', port=27017)
     db = client['tweets']
     collection = db['uk_brexit']
 
-    res = collection.find({}, {'_id' : False})
-    res = list(res[:])[:10]
+    res = collection.find({}, {'_id': False})
+    res = list(res[:])[:200]
 
     clf = BoWClassifier()
 
-    tweets = [clf.clean_text(tweet['text']) for tweet in res]
-    sentiments = [clf.get_sentiment(tweet) for tweet in tweets]
+    clean_tweets = [clf.clean_text(tweet['text']) for tweet in res]
+    sentiments = [clf.get_sentiment(tweet) for tweet in clean_tweets]
+    sentiments_counter = Counter(sentiments)
+
+    # Sort keys for the barplot palette
+    key_order = ['positive', 'neutral', 'negative']
+    sentiments_counter = {k : sentiments_counter[k] for k in key_order}
+
+    plot_wordcloud(clean_tweets)
+
+    sentiments_df = pd.DataFrame({
+        'Sentiment': list(sentiments_counter.keys()),
+        'Count': list(sentiments_counter.values()),
+    })
+    ax = sns.barplot(
+        x='Sentiment', y='Count', data=sentiments_df, palette='RdBu_r'
+    )
+    plt.show()
